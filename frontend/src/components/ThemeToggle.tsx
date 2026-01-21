77b12d9da0b1975d6b075098d11dd49b2bda2e55
DOC <hasanov326@mail.ru>
2026-01-21 22:36:32 +0400

    formating tabulation

--- frontend/src/components/ThemeToggle.tsx
+++ frontend/src/components/ThemeToggle.tsx
@@ -4,11 +4,11 @@ import LightModeIcon from '@mui/icons-material/LightMode';
 import { useThemeStore } from '../store/themeStore';
 
 export const ThemeToggle = () => {
-  const { mode, toggle } = useThemeStore();
+    const { mode, toggle } = useThemeStore();
 
-  return (
-    <IconButton onClick={toggle}>
-      {mode === 'light' ? <DarkModeIcon /> : <LightModeIcon />}
-    </IconButton>
-  );
+    return (
+        <IconButton onClick={toggle}>
+            {mode === 'light' ? <DarkModeIcon /> : <LightModeIcon />}
+        </IconButton>
+    );
 };
