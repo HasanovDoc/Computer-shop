3ad3705824f62780cb1154758aa12d75cd7b2766
DOC <hasanov326@mail.ru>
2026-01-22 16:44:31 +0400

    fix: add product in db

--- backend/ComputerStore.Api/Models/Product.cs
+++ backend/ComputerStore.Api/Models/Product.cs
@@ -7,11 +7,12 @@ public class Product
     public int Id { get; set; }
     public string Name { get; set; } = null!;
     public int ProductTypeId { get; set; }
-    public ProductType ProductType { get; set; } = null!;
+    
+    public ProductType? ProductType { get; set; }
 
     public decimal PriceBuy { get; set; }
     public decimal PriceSell { get; set; }
 
-    public string Specs { get; set; } = "{}"; // JSON
+    public string Specs { get; set; } = "{}";
     public bool IsSold { get; set; }
 }
